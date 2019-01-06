12f6c205bd417e2eea5c7ad2f8ca6a91b76302e1
Luke Savage <lsava22@hotmail.com>
2019-01-06 21:20:18 +1000

    PROTO-1 Redid Left/Right movement using raycasts with box collider

--- DwarfGame - Unity/Assets/Scripts/PlayerController.cs
+++ DwarfGame - Unity/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ namespace DwarfGame
     {
         public PlayerVariables PlayerVars;
         public GameObject BodySprite;
-        
+        public LayerMask CollisionMask;
+
         private Tilemap _terrain;
         private Rigidbody2D _rb;
         private BoxCollider2D _col;
@@ -39,37 +40,94 @@ namespace DwarfGame
     
         private void Update()
         {
-            if(_isJumping)
-            {
-                if(transform.position.y >= _targetJumpHeight || HasHitCeiling())
-                {
-                    _isJumping = false;
-                }
-                else
-                {
-                    _rb.velocity = new Vector2(_rb.velocity.x, PlayerVars.JumpSpeedBase);
-                }
-            }
-            else
-            {
-                _rb.velocity = new Vector2(_rb.velocity.x, -PlayerVars.FallSpeedBase);
-            
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    _targetJumpHeight = transform.position.y + (PlayerVars.JumpHeightBase * PlayerVars.JumpHeightModifier[PlayerVars.JumpHeightCurModifier]) + 0.2f;
-                    _isJumping = true;
-                }
-            }
+            //if(_isJumping)
+            //{
+            //    if(transform.position.y >= _targetJumpHeight || HasHitCeiling())
+            //    {
+            //        _isJumping = false;
+            //    }
+            //    else
+            //    {
+            //        _rb.velocity = new Vector2(_rb.velocity.x, PlayerVars.JumpSpeedBase);
+            //    }
+            //}
+            //else
+            //{
+            //    _rb.velocity = new Vector2(_rb.velocity.x, -PlayerVars.FallSpeedBase);
+            //
+            //    if (Input.GetKey(KeyCode.Space))
+            //    {
+            //        _targetJumpHeight = transform.position.y + (PlayerVars.JumpHeightBase * PlayerVars.JumpHeightModifier[PlayerVars.JumpHeightCurModifier]) + 0.2f;
+            //        _isJumping = true;
+            //    }
+            //}
             
             float input = Input.GetAxisRaw("Horizontal");
             if (input != 0)
             {
-                // TODO: figure out how to access the tiles position from within the tile.
-                _rb.velocity = new Vector2(input * (PlayerVars.MoveSpeedBase * PlayerVars.MovespeedMultiplier[PlayerVars.MoveSpeedCurModifier]), _rb.velocity.y);
+                // TODO: Try moving these into FixedUpdate()
+                float distance = PlayerVars.MoveSpeedBase;
+                if (input > 0)
+                {
+                    float rayDistance = distance + _col.bounds.extents.x;
+
+                    Vector2 center = _col.bounds.center;
+                    Vector2 upper = new Vector2(center.x, center.y + _col.bounds.extents.y);
+                    Vector2 lower = new Vector2(center.x, center.y - _col.bounds.extents.y);
+
+                    RaycastHit2D[] hits = new RaycastHit2D[3];
+                    hits[0] = Physics2D.Raycast(upper, Vector2.right, rayDistance, CollisionMask);
+                    hits[1] = Physics2D.Raycast(center, Vector2.right, rayDistance, CollisionMask);
+                    hits[2] = Physics2D.Raycast(lower, Vector2.right, rayDistance, CollisionMask);
+
+                    float hitDistance = rayDistance + 1f;
+                    foreach (RaycastHit2D hit in hits)
+                    {
+                        if (hit.collider != null)
+                        {
+                            hitDistance = hit.distance < hitDistance ? hit.distance : hitDistance;
+                        }
+                    }
+
+                    if (hitDistance < rayDistance)
+                    {
+                        distance = hitDistance - _col.bounds.extents.x;
+                    }
+                }
+                else
+                {
+                    float rayDistance = distance + _col.bounds.extents.x;
+
+                    Vector2 center = _col.bounds.center;
+                    Vector2 upper = new Vector2(center.x, center.y + _col.bounds.extents.y);
+                    Vector2 lower = new Vector2(center.x, center.y - _col.bounds.extents.y);
+
+                    RaycastHit2D[] hits = new RaycastHit2D[3];
+                    hits[0] = Physics2D.Raycast(upper, Vector2.left, rayDistance, CollisionMask);
+                    hits[1] = Physics2D.Raycast(center, Vector2.left, rayDistance, CollisionMask);
+                    hits[2] = Physics2D.Raycast(lower, Vector2.left, rayDistance, CollisionMask);
+
+                    float hitDistance = rayDistance + 1f;
+                    foreach (RaycastHit2D hit in hits)
+                    {
+                        if (hit.collider != null)
+                        {
+                            hitDistance = hit.distance < hitDistance ? hit.distance : hitDistance;
+                        }
+                    }
+
+                    if (hitDistance < rayDistance)
+                    {
+                        distance = hitDistance - _col.bounds.extents.x;
+                    }
+                    distance = -distance;
+                }
+
+                transform.Translate(Vector3.right * distance);
             }
             else
             {
-                _rb.velocity = new Vector2(0, _rb.velocity.y);
+                //_rb.velocity = new Vector2(0, _rb.velocity.y);
             }
             
             if(Input.GetMouseButtonDown(0))
