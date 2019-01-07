e1c7282fbb8f507ef6c3fb0d6f20f1a332e6cc31
Luke Savage <lsava22@hotmail.com>
2019-01-07 22:21:03 +1000

    PROTO-1 Moved raycasts and translations into FixedUpdate()
    -Added debug rays to show existing raycasts

--- DwarfGame - Unity/Assets/Scripts/PlayerController.cs
+++ DwarfGame - Unity/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.Tilemaps;
 
 namespace DwarfGame
@@ -19,6 +20,7 @@ namespace DwarfGame
         private float _targetJumpHeight;
 
         private Bounds _bounds;
+        private float _moveInput;
 
         public Bounds Bounds
         {
@@ -42,18 +44,55 @@ namespace DwarfGame
         {
             if(_isJumping)
             {
-                //if(transform.position.y >= _targetJumpHeight || HasHitCeiling())
-                //{
-                //    _isJumping = false;
-                //}
-                //else
-                //{
-                //    _rb.velocity = new Vector2(_rb.velocity.x, PlayerVars.JumpSpeedBase);
-                //}
+                
             }
             else
             {
-                // TODO: Why does this block of code invert left/right controls and character speed???
+                if (Input.GetKey(KeyCode.Space)) // TODO: Only allow jumping when grounded
+                {
+                    _targetJumpHeight = transform.position.y + (PlayerVars.JumpHeightBase * PlayerVars.JumpHeightModifier[PlayerVars.JumpHeightCurModifier]) + 0.2f;
+                    _isJumping = true;
+                }
+            }
+            
+            _moveInput = Input.GetAxisRaw("Horizontal");
+            
+            if(Input.GetMouseButtonDown(0))
+            {
+                TilemapManager.Instance.TerrainTilemap.SetTile(TilemapManager.Instance.TerrainTilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition)), null);
+    
+                //TileBase tile = TilemapManager.Instance.TerrainTilemap.GetTile(TilemapManager.Instance.TerrainTilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+                //if (tile != null)
+                //{
+                //    Debug.Log(tile.name);
+                //}
+            }
+    
+            if(_moveInput < 0)
+            {
+                BodySprite.transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if(_moveInput > 0)
+            {
+                BodySprite.transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if(_isJumping)
+            {
+                if(transform.position.y >= _targetJumpHeight || HasHitCeiling())
+                {
+                    _isJumping = false;
+                }
+                else
+                {
+                    transform.Translate(Vector3.up * PlayerVars.JumpSpeedBase);
+                }
+            }
+            else
+            {
                 float distance = PlayerVars.FallSpeedBase;
                 float rayDistance = distance + _col.bounds.extents.y;
                 
@@ -62,14 +101,14 @@ namespace DwarfGame
                 Vector2 right = new Vector2(center.x + _col.bounds.extents.x, center.y);
                 
                 RaycastHit2D[] hits = new RaycastHit2D[3];
-                hits[0] = Physics2D.Raycast(left, Vector2.down, rayDistance, CollisionMask);
-                hits[1] = Physics2D.Raycast(center, Vector2.down, rayDistance, CollisionMask);
-                hits[2] = Physics2D.Raycast(right, Vector2.down, rayDistance, CollisionMask);
+                hits[0] = Raycast(left, Vector2.down, rayDistance, CollisionMask, Color.blue);
+                hits[1] = Raycast(center, Vector2.down, rayDistance, CollisionMask, Color.blue);
+                hits[2] = Raycast(right, Vector2.down, rayDistance, CollisionMask, Color.blue);
                 
                 float hitDistance = rayDistance + 1f;
                 foreach (RaycastHit2D hit in hits)
                 {
-                    if (hit.collider != null)
+                    if (hit.collider != null && hit.distance > 0) // 0 distance removes hits on edges of our collider
                     {
                         hitDistance = hit.distance < hitDistance ? hit.distance : hitDistance;
                     }
@@ -79,25 +118,17 @@ namespace DwarfGame
                 {
                     distance = hitDistance - _col.bounds.extents.y;
                 }
-
+            
                 if (distance > 0)
                 {
                     transform.Translate(Vector3.down * distance);   
                 }
-                
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    _targetJumpHeight = transform.position.y + (PlayerVars.JumpHeightBase * PlayerVars.JumpHeightModifier[PlayerVars.JumpHeightCurModifier]) + 0.2f;
-                    _isJumping = true;
-                }
             }
-            
-            float input = Input.GetAxisRaw("Horizontal");
-            if (input != 0)
+
+            if (_moveInput != 0)
             {
-                // TODO: Try moving these into FixedUpdate()
                 float distance = PlayerVars.MoveSpeedBase;
-                if (input > 0)
+                if (_moveInput > 0)
                 {
                     float rayDistance = distance + _col.bounds.extents.x;
 
@@ -106,14 +137,14 @@ namespace DwarfGame
                     Vector2 lower = new Vector2(center.x, center.y - _col.bounds.extents.y);
 
                     RaycastHit2D[] hits = new RaycastHit2D[3];
-                    hits[0] = Physics2D.Raycast(upper, Vector2.right, rayDistance, CollisionMask);
-                    hits[1] = Physics2D.Raycast(center, Vector2.right, rayDistance, CollisionMask);
-                    hits[2] = Physics2D.Raycast(lower, Vector2.right, rayDistance, CollisionMask);
+                    hits[0] = Raycast(upper, Vector2.right, rayDistance, CollisionMask, Color.cyan);
+                    hits[1] = Raycast(center, Vector2.right, rayDistance, CollisionMask, Color.cyan);
+                    hits[2] = Raycast(lower, Vector2.right, rayDistance, CollisionMask, Color.cyan);
 
                     float hitDistance = rayDistance + 1f;
                     foreach (RaycastHit2D hit in hits)
                     {
-                        if (hit.collider != null)
+                        if (hit.collider != null && hit.distance > 0) // TODO: Figure out how to handle raycasts on adjacent floor/wall
                         {
                             hitDistance = hit.distance < hitDistance ? hit.distance : hitDistance;
                         }
@@ -133,14 +164,14 @@ namespace DwarfGame
                     Vector2 lower = new Vector2(center.x, center.y - _col.bounds.extents.y);
 
                     RaycastHit2D[] hits = new RaycastHit2D[3];
-                    hits[0] = Physics2D.Raycast(upper, Vector2.left, rayDistance, CollisionMask);
-                    hits[1] = Physics2D.Raycast(center, Vector2.left, rayDistance, CollisionMask);
-                    hits[2] = Physics2D.Raycast(lower, Vector2.left, rayDistance, CollisionMask);
+                    hits[0] = Raycast(upper, Vector2.left, rayDistance, CollisionMask, Color.cyan);
+                    hits[1] = Raycast(center, Vector2.left, rayDistance, CollisionMask, Color.cyan);
+                    hits[2] = Raycast(lower, Vector2.left, rayDistance, CollisionMask, Color.cyan);
 
                     float hitDistance = rayDistance + 1f;
                     foreach (RaycastHit2D hit in hits)
                     {
-                        if (hit.collider != null)
+                        if (hit.collider != null && hit.distance > 0) // 0 distance removes hits on edges of our collider
                         {
                             hitDistance = hit.distance < hitDistance ? hit.distance : hitDistance;
                         }
@@ -150,34 +181,11 @@ namespace DwarfGame
                     {
                         distance = hitDistance - _col.bounds.extents.x;
                     }
+
                     distance = -distance;
                 }
 
                 transform.Translate(Vector3.right * distance);
-            }
-            else
-            {
-                //_rb.velocity = new Vector2(0, _rb.velocity.y);
-            }
-            
-            if(Input.GetMouseButtonDown(0))
-            {
-                TilemapManager.Instance.TerrainTilemap.SetTile(TilemapManager.Instance.TerrainTilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition)), null);
-    
-                //TileBase tile = TilemapManager.Instance.TerrainTilemap.GetTile(TilemapManager.Instance.TerrainTilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
-                //if (tile != null)
-                //{
-                //    Debug.Log(tile.name);
-                //}
-            }
-    
-            if(input < 0)
-            {
-                BodySprite.transform.localScale = new Vector3(-1, 1, 1);
-            }
-            else if(input > 0)
-            {
-                BodySprite.transform.localScale = new Vector3(1, 1, 1);
             }
         }
         
@@ -190,6 +198,13 @@ namespace DwarfGame
         {
             Vector2 fallingThreshold = Vector2.down * 0.01f;
             return !_terrain.HasTile(_terrain.WorldToCell(Bounds.BottomLeft + fallingThreshold)) && !_terrain.HasTile(_terrain.WorldToCell(Bounds.BottomRight + fallingThreshold));
+        }
+
+        // TODO: Put this into a util class, add a bool for drawing rays
+        public static RaycastHit2D Raycast(Vector2 origin, Vector2 direction, float distance, LayerMask mask, Color colour)
+        {
+            Debug.DrawRay(origin, direction * distance, colour);
+            return Physics2D.Raycast(origin, direction, distance, mask);
         }
     }
 
