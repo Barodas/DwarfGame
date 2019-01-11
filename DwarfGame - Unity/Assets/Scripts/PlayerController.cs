2c13cd77af566978d2e565c8696b474d0d5d2471
Luke Savage <lsava22@hotmail.com>
2019-01-12 06:35:50 +1000

    PROTO-1 Cleaned up center and extent references in PlayerController.cs
    -Moved Raycast and MultiRaycast calls into Utils.cs

--- DwarfGame - Unity/Assets/Scripts/PlayerController.cs
+++ DwarfGame - Unity/Assets/Scripts/PlayerController.cs
@@ -19,41 +19,19 @@ namespace DwarfGame
         public LayerMask CollisionMask;
 
         private Tilemap _terrain;
-        private Rigidbody2D _rb;
         private BoxCollider2D _col;
 
-        private VerticalState _verticalState = VerticalState.Grounded;
+        private VerticalState State { get; set; } = VerticalState.Grounded;
 
-        private VerticalState State
-        {
-            get { return _verticalState; }
-            set
-            {
-                _verticalState = value;
-                Debug.LogFormat("State changed to {0}", value);
-            }
-        }
-    
         private float _targetJumpHeight;
-
-        private Bounds _bounds;
         private float _moveInput;
 
-        public Bounds Bounds
-        {
-            get
-            {
-                _bounds.Center = transform.position;
-                return _bounds;
-            }
-            private set => _bounds = value;
-        }
-    
+        private Vector2 Center => _col.bounds.center;
+        private Vector2 Extents => _col.bounds.extents;
+
         private void Start()
         {
             _terrain = TilemapManager.Instance.TerrainTilemap;
-            Bounds = new Bounds(transform.position);
-            _rb = GetComponent<Rigidbody2D>();
             _col = GetComponent<BoxCollider2D>();
         }
     
@@ -64,7 +42,7 @@ namespace DwarfGame
                 case VerticalState.Grounded:
                     if (Input.GetKey(KeyCode.Space))
                     {
-                        _targetJumpHeight = _col.bounds.center.y + _col.bounds.extents.y + (PlayerVars.JumpHeightBase * PlayerVars.JumpHeightModifier[PlayerVars.JumpHeightCurModifier]) + 0.2f;
+                        _targetJumpHeight = Center.y + Extents.y + (PlayerVars.JumpHeightBase * PlayerVars.JumpHeightModifier[PlayerVars.JumpHeightCurModifier]) + 0.2f;
                         State = VerticalState.Jumping;
                     }
                     break;
@@ -107,14 +85,14 @@ namespace DwarfGame
                 case VerticalState.Grounded:
                 {
                     // Check if we should be falling 
-                    float groundDistance = MultiRaycast(Vector2.down, PlayerVars.FallSpeedBase + _col.bounds.extents.y,
+                    float groundDistance = Utils.MultiRaycast(Vector2.down, PlayerVars.FallSpeedBase + Extents.y,
                         new Vector2[]
                         {
-                            new Vector2(_col.bounds.center.x - _col.bounds.extents.x + EdgeRayOffset, _col.bounds.center.y),
-                            _col.bounds.center,
-                            new Vector2(_col.bounds.center.x + _col.bounds.extents.x - EdgeRayOffset, _col.bounds.center.y)
-                        });
-                    if (groundDistance - _col.bounds.extents.y > 0)
+                            new Vector2(_col.bounds.center.x - Extents.x + EdgeRayOffset, Center.y),
+                            Center,
+                            new Vector2(Center.x + Extents.x - EdgeRayOffset, Center.y)
+                        }, CollisionMask, Color.blue);
+                    if (groundDistance - Extents.y > 0)
                     {
                         State = VerticalState.Falling;
                     }
@@ -122,42 +100,42 @@ namespace DwarfGame
                 }
                 case VerticalState.Jumping:
                     // Clamp distance to targetHeight
-                    float targetDistance = PlayerVars.JumpSpeedBase + _col.bounds.extents.y;
+                    float targetDistance = PlayerVars.JumpSpeedBase + Extents.y;
                     bool reachedPeak = false;
-                    if (_col.bounds.center.y + targetDistance >= _targetJumpHeight)
+                    if (Center.y + targetDistance >= _targetJumpHeight)
                     {
-                        targetDistance = _targetJumpHeight - _col.bounds.center.y;
+                        targetDistance = _targetJumpHeight - Center.y;
                         reachedPeak = true;
                     }
                     // Check for collision if return from raycasts is lower than targetdistance we must have hit something, set falling
-                    float jumpDistance = MultiRaycast(Vector2.up, targetDistance,
+                    float jumpDistance = Utils.MultiRaycast(Vector2.up, targetDistance,
                         new Vector2[]
                         {
-                            new Vector2(_col.bounds.center.x - _col.bounds.extents.x + EdgeRayOffset, _col.bounds.center.y),
-                            _col.bounds.center,
-                            new Vector2(_col.bounds.center.x + _col.bounds.extents.x - EdgeRayOffset, _col.bounds.center.y)
-                        });
+                            new Vector2(Center.x - Extents.x + EdgeRayOffset, Center.y),
+                            Center,
+                            new Vector2(Center.x + Extents.x - EdgeRayOffset, Center.y)
+                        }, CollisionMask, Color.blue);
                     // Change state to falling 
                     if (jumpDistance < targetDistance || reachedPeak)
                     {
                         State = VerticalState.Falling;
                     }
                     // apply jumpDistance
-                    jumpDistance -= _col.bounds.extents.y;
+                    jumpDistance -= Extents.y;
                     if (jumpDistance > 0)
                     {
                         transform.Translate(Vector2.up * jumpDistance);
                     }
                     break;
                 case VerticalState.Falling:
-                    float fallingDistance = MultiRaycast(Vector2.down, PlayerVars.FallSpeedBase + _col.bounds.extents.y,
+                    float fallingDistance = Utils.MultiRaycast(Vector2.down, PlayerVars.FallSpeedBase + Extents.y,
                         new Vector2[]
                         {
-                            new Vector2(_col.bounds.center.x - _col.bounds.extents.x + EdgeRayOffset, _col.bounds.center.y),
-                            _col.bounds.center,
-                            new Vector2(_col.bounds.center.x + _col.bounds.extents.x - EdgeRayOffset, _col.bounds.center.y)
-                        });
-                    fallingDistance -= _col.bounds.extents.y;
+                            new Vector2(Center.x - Extents.x + EdgeRayOffset, Center.y),
+                            Center,
+                            new Vector2(Center.x + Extents.x - EdgeRayOffset, Center.y)
+                        }, CollisionMask, Color.blue);
+                    fallingDistance -= Extents.y;
                     if (fallingDistance >= 0.001f)
                     {
                         transform.Translate(Vector2.down * fallingDistance);
@@ -173,53 +151,20 @@ namespace DwarfGame
             // Horizontal state
             if (_moveInput != 0)
             {
-                float distance = PlayerVars.MoveSpeedBase + _col.bounds.extents.x;
+                float distance = PlayerVars.MoveSpeedBase + Extents.x;
                 Vector2 direction = _moveInput > 0 ? Vector2.right : Vector2.left;
 
-                distance = MultiRaycast(direction, distance,
+                distance = Utils.MultiRaycast(direction, distance,
                     new Vector2[]
                     {
-                        new Vector2(_col.bounds.center.x, _col.bounds.center.y + _col.bounds.extents.y - EdgeRayOffset),
-                        _col.bounds.center,
-                        new Vector2(_col.bounds.center.x, _col.bounds.center.y - _col.bounds.extents.y + EdgeRayOffset),
-                    });
-                distance -= _col.bounds.extents.x;
+                        new Vector2(Center.x, Center.y + Extents.y - EdgeRayOffset),
+                        Center,
+                        new Vector2(Center.x, Center.y - Extents.y + EdgeRayOffset),
+                    }, CollisionMask, Color.blue);
+                distance -= Extents.x;
 
                 transform.Translate(direction * distance);
             }
-        }
-
-        private float MultiRaycast(Vector2 direction, float distance, Vector2[] rayOrigins)
-        {
-            float rayDistance = distance;
-            RaycastHit2D[] hits = new RaycastHit2D[rayOrigins.Length];
-            for (int i = 0; i < rayOrigins.Length; i++)
-            {
-                hits[i] = Raycast(rayOrigins[i], direction, rayDistance, CollisionMask, Color.blue);
-            }
-
-            float hitDistance = rayDistance + 1f;
-            foreach (RaycastHit2D hit in hits)
-            {
-                if (hit.collider != null && hit.distance > 0) // 0 distance removes hits on edges of our collider
-                {
-                    hitDistance = hit.distance < hitDistance ? hit.distance : hitDistance;
-                }
-            }
-                
-            if (hitDistance < rayDistance)
-            {
-                distance = hitDistance;
-            }
-
-            return distance;
-        }
-
-        // TODO: Put this into a util class, add a bool for drawing rays
-        public static RaycastHit2D Raycast(Vector2 origin, Vector2 direction, float distance, LayerMask mask, Color colour)
-        {
-            Debug.DrawRay(origin, direction * distance, colour);
-            return Physics2D.Raycast(origin, direction, distance, mask);
         }
     }
 
